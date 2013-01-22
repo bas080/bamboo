1985c25c3e92801f00833960cdbd6a2ee3c9b77f
Bas <bassimhuis@gmail.com>
2013-01-22 20:20:26 +0100

    Added dry bamboo

--- init.lua
+++ init.lua
@@ -1,6 +1,34 @@
 local mod_name = "bamboo"
 local average_height = 4
 
+minetest.register_craft({
+	output = 'bamboo:block',
+	recipe = {
+		{'bamboo:item', 'bamboo:item', ''},
+		{'bamboo:item', 'bamboo:item', ''},
+		{'', '', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'bamboo:block_dry',
+	recipe = {
+		{'bamboo:item_dry', 'bamboo:item_dry', ''},
+		{'bamboo:item_dry', 'bamboo:item_dry', ''},
+		{'', '', ''},
+	}
+})
+
+minetest.register_craftitem("bamboo:item", {
+	description = "Bamboo Damp",
+	inventory_image = "bamboo_item.png",
+})
+
+minetest.register_craftitem("bamboo:item_dry", {
+	description = "Bamboo Dry",
+	inventory_image = "bamboo_item_dry.png",
+})
+
 minetest.register_node("bamboo:bamboo_top", {
   tile_images = {"bamboo_top.png"},
   inventory_image = "bamboo_top.png",
@@ -10,7 +38,7 @@ minetest.register_node("bamboo:bamboo_top", {
   visual_scale = 1.4,
   paramtype = "light",
   walkable = false,
-  groups = { snappy = 3,flammable=2, floored=1},
+  groups = { snappy = 3,flammable=2, attached_node=1},
   sounds = default.node_sound_leaves_defaults(),
   selection_box = {
     type = "fixed",
@@ -20,11 +48,11 @@ minetest.register_node("bamboo:bamboo_top", {
   max_items = 2,
   items = {
     {
-      items = {'bamboo:bamboo_sapling'},
-      rarity = 5,
+      items = {'bamboo:sapling'},
+      rarity = 2,
       },
       {
-      items = {"bamboo:bamboo"},
+      items = {"bamboo:item"},
       }
     }
   },
@@ -33,42 +61,85 @@ minetest.register_node("bamboo:bamboo_top", {
 minetest.register_node("bamboo:bamboo", {
   tile_images = {"bamboo.png"},
   inventory_image = "bamboo.png",
-  description = "bamboo",
+  description = "Bamboo",
   drawtype = "plantlike",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      {-5/16, -0.5, -5/16, -2/16, 0.5, -2/16},  
+      {5/16, -0.5, 5/16, 2/16, 0.5, 2/16},
+    },
+  },
+  selection_box = {
+    type = "fixed",
+    fixed = { -0.33, -0.5, -0.33, 0.33, 0.5, 0.33 },
+  },
   sunlight_propagates = true,
   visual_scale = 1.0,
   paramtype = "light",
   walkable = false,
-  groups = { snappy = 3,flammable=2, floored=1},
+  groups = { snappy = 3,flammable=2, attached_node=1},
   sounds = default.node_sound_leaves_defaults(),
   drop = {
   max_items = 2,
   items = {
     {
-      items = {'bamboo:bamboo_sapling'},
+      items = {'bamboo:sapling'},
       rarity = 5,
       },
       {
-      items = {"bamboo:bamboo"},
+      items = {"bamboo:item"},
       }
     }
   },
+})
+
+minetest.register_node("bamboo:bamboo_dry", {
+  tile_images = {"bamboo_dry.png"},
+  inventory_image = "bamboo_dry.png",
+  description = "Bamboo",
+  drawtype = "plantlike",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      {-5/16, -0.5, -5/16, -2/16, 0.5, -2/16},  
+      {5/16, -0.5, 5/16, 2/16, 0.5, 2/16},
+    },
+  },
   selection_box = {
     type = "fixed",
-    fixed = { -0.25, -0.5, -0.25, 0.25, 0.5, 0.25 },
+    fixed = { -0.33, -0.5, -0.33, 0.33, 0.5, 0.33 },
+  },
+  sunlight_propagates = true,
+  visual_scale = 1.0,
+  paramtype = "light",
+  walkable = false,
+  groups = { snappy = 3,flammable=2, attached_node=1},
+  sounds = default.node_sound_leaves_defaults(),
+  drop = {
+  max_items = 2,
+  items = {
+    {
+      items = {'bamboo:sapling'},
+      rarity = 10,
+      },
+      {
+      items = {"bamboo:item_dry"},
+      }
+    }
   },
 })
 
-minetest.register_node("bamboo:bamboo_sapling", {
+minetest.register_node("bamboo:sapling", {
   tile_images = {"bamboo_top.png"},
   inventory_image = "bamboo_top.png",
-  description = "bamboo",
+  description = "Bamboo Cutting",
   drawtype = "plantlike",
   sunlight_propagates = true,
   visual_scale = 0.9,
   paramtype = "light",
   walkable = false,
-  groups = { snappy = 3,flammable=2, floored=1},
+  groups = { snappy = 3,flammable=2, attached_node=1},
   sounds = default.node_sound_leaves_defaults(),
   selection_box = {
     type = "fixed",
@@ -94,7 +165,16 @@ minetest.register_abm({
 })
 
 minetest.register_abm({
-  nodenames = {"bamboo:bamboo_sapling"},
+  nodenames = {"bamboo:bamboo"},
+  interval = 300,
+  chance = 2,
+  action = function(pos)
+    minetest.env:add_node(pos, {name="bamboo:bamboo_dry"})
+  end,
+})
+
+minetest.register_abm({
+  nodenames = {"bamboo:sapling"},
   interval = 30,
   chance = 10,
   action = function(pos)
@@ -106,6 +186,22 @@ minetest.register_abm({
     end
     
   end,
+})
+
+minetest.register_node("bamboo:block", {
+	description = "Damp Bamboo Block",
+	tiles = {"bamboo_node.png"},
+	is_ground_content = true,
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("bamboo:block_dry", {
+	description = "Dry Bamboo Block",
+	tiles = {"bamboo_node_dry.png"},
+	is_ground_content = true,
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+	sounds = default.node_sound_wood_defaults(),
 })
 
 print("[Bamboo] Loaded!")
